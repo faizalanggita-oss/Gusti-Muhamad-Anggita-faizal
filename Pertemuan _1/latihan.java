467c44b7387a3f115400564afe8b95a34f2df073
Gusti Muhamad Anggita Faizal <faizalanggita@gmail.com>
2026-04-06 16:15:16 +0700

    Add latihan.java for Pertemuan 1 assignment

---
+++ Pertemuan _1/latihan.java
@@ -0,0 +1,9 @@
+// Nama: Gusti Muhamad A.F
+// NIM : 241011402467
+// Kelas: 04TPLP005
+
+public class latihan {
+    public static void main(String[] args) {
+        System.out.println("Tugas Pertemuan 1 berhasil dibuat!");
+    }
+}
